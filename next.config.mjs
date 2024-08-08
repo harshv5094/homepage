2d557dda9a71122b743967bef71d28d56425a24b
harshv5094 (Fedora Personal PC) <harshv5094@gmail.com>
2024-08-08 22:04:23 +0530

    feat(next.config.mjs): removing experimental feature

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    esmExternals: false
-  }
+  reactStrictMode: true
 }
 
 export default nextConfig

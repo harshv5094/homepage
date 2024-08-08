5d68879117a8b0bb19d883de9f9ec638f4624035
harshv5094 (Fedora Personal PC) <harshv5094@gmail.com>
2024-08-08 12:39:48 +0530

    feat(navbar): adding responsive navbar design

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,3 +1,16 @@
-function Projects() {}
+'use client'
+import Main from '@/components/layout/main'
+import { useEffect } from 'react'
+
+function Projects() {
+  useEffect(() => {
+    document.title = 'Harsh Vyapari - Projects'
+  }, [])
+  return (
+    <Main>
+      <h1> This is the projects page</h1>
+    </Main>
+  )
+}
 
 export default Projects
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,8 +1,44 @@
-import { Box, Container, Flex, Heading, Stack } from '@chakra-ui/react'
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Stack,
+  useColorModeValue
+} from '@chakra-ui/react'
 import Logo from './logo'
 import ThemeToggleButton from './theme-toggle-button'
+import { Link } from '@chakra-ui/next-js'
+import { forwardRef } from 'react'
+import { HamburgerIcon } from '@chakra-ui/icons'
 
-const Navbar = () => {
+const LinkItem = ({ href, path, target, children, ...props }) => {
+  const active = path === href
+  const inactiveColor = useColorModeValue('gray.800', 'whiteAlpha.900')
+  return (
+    <Link
+      href={href}
+      scroll={false}
+      p={2}
+      bg={active ? 'dodgerblue' : undefined}
+      color={active ? '#202023' : inactiveColor}
+      target={target}
+      {...props}
+    >
+      {children}
+    </Link>
+  )
+}
+
+const MenuLink = forwardRef((props, ref) => <Link ref={ref} {...props} />)
+
+const Navbar = props => {
+  const { path } = props
   return (
     <Box as="nav" position={'fixed'} w={'full'} zIndex={2}>
       <Container display={'flex'} p={2} maxW="container.xl">
@@ -19,10 +55,33 @@ const Navbar = () => {
           alignItems={'center'}
           flexGrow={1}
           mt={{ base: 4, md: 0 }}
-        ></Stack>
+        >
+          <LinkItem href={'/projects'} path={path}>
+            Projects
+          </LinkItem>
+        </Stack>
 
         <Box flex={1} align="right">
           <ThemeToggleButton />
+
+          <Box ml={1} display={{ base: 'inline-block', md: 'none' }}>
+            <Menu isLazy id="navbar-menu">
+              <MenuButton
+                as={IconButton}
+                icon={<HamburgerIcon />}
+                variant={'outline'}
+                aria-label="Options"
+              />
+              <MenuList>
+                <MenuItem as={MenuLink} href="/">
+                  About
+                </MenuItem>
+                <MenuItem as={MenuLink} href="/projects">
+                  Projects
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Box>
         </Box>
       </Container>
     </Box>

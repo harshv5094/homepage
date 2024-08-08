3e8a4554e06c051e8e8c27afdef3ff81c9387565
harshv5094 (Fedora Personal PC) <harshv5094@gmail.com>
2024-08-08 17:48:28 +0530

    feat(navbar): adding bg color to active link and also adding contact-me page link

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -16,10 +16,17 @@ import ThemeToggleButton from './theme-toggle-button'
 import { Link } from '@chakra-ui/next-js'
 import { forwardRef } from 'react'
 import { HamburgerIcon } from '@chakra-ui/icons'
+import { usePathname } from 'next/navigation'
 
-const LinkItem = ({ href, path, target, children, ...props }) => {
-  const active = path === href
-  const inactiveColor = useColorModeValue('gray.800', 'whiteAlpha.900')
+const LinkItem = ({ href, children, ...props }) => {
+  // HACK: usePathname for getting current router path
+  const pathname = usePathname()
+
+  // HACK: validating pathname with user href prop value
+  const isActive = pathname === href
+
+  // NOTE: a common hook variable for changing text color
+  const colorHook = useColorModeValue('white', 'black')
   return (
     <Link
       href={href}
@@ -27,11 +34,10 @@ const LinkItem = ({ href, path, target, children, ...props }) => {
       p={2}
       _hover={{
         bg: 'dodgerblue',
-        color: useColorModeValue('white', 'black')
+        color: colorHook
       }}
-      bg={active ? 'dodgerblue' : undefined}
-      color={active ? '#202023' : inactiveColor}
-      target={target}
+      bg={isActive && 'dodgerblue'}
+      color={isActive && colorHook}
       {...props}
     >
       {children}
@@ -41,14 +47,14 @@ const LinkItem = ({ href, path, target, children, ...props }) => {
 
 const MenuLink = forwardRef((props, ref) => <Link ref={ref} {...props} />)
 
-const Navbar = props => {
-  const { path } = props
+const Navbar = () => {
   return (
     <Box
       as="nav"
       position={'fixed'}
       w={'full'}
       css={{ backdropFilter: 'blur(10px)' }}
+      bg={useColorModeValue('#f5f5f540', '#202111B80')}
       zIndex={2}
     >
       <Container display={'flex'} p={2} maxW="container.xl">
@@ -59,6 +65,8 @@ const Navbar = props => {
         </Flex>
 
         <Stack
+          flex={1}
+          align="right"
           direction={{ base: 'column', md: 'row' }}
           display={{ base: 'none', md: 'flex' }}
           width={{ base: 'full', md: 'auto' }}
@@ -66,9 +74,8 @@ const Navbar = props => {
           flexGrow={1}
           mt={{ base: 4, md: 0 }}
         >
-          <LinkItem href={'/projects'} path={path}>
-            Projects
-          </LinkItem>
+          <LinkItem href={'/projects'}>Projects</LinkItem>
+          <LinkItem href={'/contact-me'}>Contact Me</LinkItem>
         </Stack>
 
         <Box flex={1} align="right">
@@ -88,6 +95,9 @@ const Navbar = props => {
                 </MenuItem>
                 <MenuItem as={MenuLink} href="/projects">
                   Projects
+                </MenuItem>
+                <MenuItem as={MenuLink} href="/contact-me">
+                  Contact Me
                 </MenuItem>
               </MenuList>
             </Menu>

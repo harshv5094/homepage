31fda532ff0f4dd8544ab35e3191cb6e5b682341
harshv5094 (Fedora Personal PC) <harshv5094@gmail.com>
2024-08-08 21:53:07 +0530

    feat(not-found.js): creating not found page

---
+++ app/not-found.js
@@ -0,0 +1,38 @@
+'use client'
+import Main from '@/components/layout/main'
+import { Box, Button, Container, Divider, Heading } from '@chakra-ui/react'
+import PageNotFound from '@/public/undraw_not_found_re_bh2e.svg'
+import { useEffect } from 'react'
+import { Image } from '@chakra-ui/next-js'
+import { IoMdHome } from 'react-icons/io'
+import { useRouter } from 'next/navigation'
+
+function NotFound() {
+  const router = useRouter()
+  useEffect(() => {
+    document.title = 'Not Found 😢'
+  }, [])
+  return (
+    <Main>
+      <Container align="center" maxW={'container.md'}>
+        <Image src={PageNotFound} boxSize={'sm'} alt="Page Not Found" />
+        <Heading as={'h1'} my={0} align="left">
+          Not Found
+        </Heading>
+        <Box align="left" mb={2}>
+          The page you are looking for, does not exist
+        </Box>
+        <Divider my={5} />
+        <Button
+          leftIcon={<IoMdHome />}
+          colorScheme={'blue'}
+          onClick={() => router.push('/')}
+        >
+          Return To Home
+        </Button>
+      </Container>
+    </Main>
+  )
+}
+
+export default NotFound

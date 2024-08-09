6f6baa4801aa0a20a3c6a27625832364b86b8b72
harshv5094 (Fedora Personal PC) <harshv5094@gmail.com>
2024-08-09 17:25:34 +0530

    chore(app/page): removing grid design

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,9 @@
 import Layout from '@/components/layout/article'
-import { GridBox, GridBoxItem } from '@/components/layout/grid-box'
 
 function Home() {
   return (
     <Layout>
-      <GridBox>
-        <GridBoxItem order={{ base: 2, md: 1 }}>Test 1</GridBoxItem>
-        <GridBoxItem order={{ base: 1, md: 2 }}>Test 2</GridBoxItem>
-      </GridBox>
+      <h1>Hello World</h1>
     </Layout>
   )
 }

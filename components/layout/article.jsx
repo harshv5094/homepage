942e6393809e4a16f4e14406d3bb1c573882dd84
harshv5094 (Fedora Personal PC) <harshv5094@gmail.com>
2024-08-09 10:27:53 +0530

    chore(article.jsx): removing exit animation
    It's getting tricky to setup exit animation in app router, so I dropped that idea

--- components/layout/article.jsx
+++ components/layout/article.jsx
@@ -3,8 +3,7 @@ import { motion } from 'framer-motion'
 
 const variants = {
   hidden: { opacity: 0, x: 0, y: 20 },
-  enter: { opacity: 1, x: 0, y: 0 },
-  exit: { opacity: 0, x: -0, y: 20 }
+  enter: { opacity: 1, x: 0, y: 0 }
 }
 
 const Layout = ({ children }) => {
@@ -12,7 +11,6 @@ const Layout = ({ children }) => {
     <motion.article
       initial={'hidden'}
       animate={'enter'}
-      exit={'exit'}
       variants={variants}
       transition={{ duration: 0.4, type: 'easeInOut' }}
       style={{ position: 'relative' }}

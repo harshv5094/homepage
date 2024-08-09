7b1f6730fd24689b471e892b6323ad71e5f2b328
harshv5094 (Fedora Personal PC) <harshv5094@gmail.com>
2024-08-09 17:24:06 +0530

    Revert "feat(app/page.jsx): setting up intro section of the website"
    This reverts commit 6102e52445d029c12a3efef3bcacf1ea47a0f51f.

--- app/page.jsx
+++ app/page.jsx
@@ -1,40 +1,13 @@
-'use client'
 import Layout from '@/components/layout/article'
 import { GridBox, GridBoxItem } from '@/components/layout/grid-box'
-import ProfilePic from '@/public/images/harsh.jpg'
-import { Image } from '@chakra-ui/next-js'
-import { Box, Heading } from '@chakra-ui/react'
 
 function Home() {
   return (
     <Layout>
-      <Box as="section" mt={{ base: 5, md: 14 }}>
-        <GridBox gap={{ base: 4, md: 0 }}>
-          <GridBoxItem
-            align={{ base: 'center', md: 'left' }}
-            order={{ base: 2, md: 1 }}
-          >
-            <Heading
-              mt={{ md: '0.5em' }}
-              as={'h1'}
-              fontSize={{ base: '2em', md: '4.2em' }}
-              lineHeight={{ md: 'sm' }}
-            >
-              Hi! I&apos;m a developer based in India
-            </Heading>
-          </GridBoxItem>
-          <GridBoxItem align={'center'} order={{ base: 1, md: 2 }}>
-            <Image
-              src={ProfilePic}
-              borderRadius={'10px'}
-              boxSize={'sm'}
-              width={{ base: '15em', md: '16em' }}
-              height={'28em'}
-              alt="Image"
-            />
-          </GridBoxItem>
-        </GridBox>
-      </Box>
+      <GridBox>
+        <GridBoxItem order={{ base: 2, md: 1 }}>Test 1</GridBoxItem>
+        <GridBoxItem order={{ base: 1, md: 2 }}>Test 2</GridBoxItem>
+      </GridBox>
     </Layout>
   )
 }

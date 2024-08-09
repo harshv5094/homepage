5156860b5e6e96927539348c85a2848ea679585d
harshv5094 (Fedora Personal PC) <harshv5094@gmail.com>
2024-08-09 17:13:56 +0530

    feat(group-box): adding additional pageProps support

--- components/layout/grid-box.jsx
+++ components/layout/grid-box.jsx
@@ -1,10 +1,11 @@
 import { Grid, GridItem } from '@chakra-ui/react'
 
-const GridBox = ({ children }) => {
+const GridBox = ({ children, ...pageProps }) => {
   return (
     <Grid
       templateRows={{ base: 'repeat(2, 1fr)', md: 'none' }}
       templateColumns={{ base: 'none', md: 'repeat(2, 1fr)' }}
+      {...pageProps}
     >
       {children}
     </Grid>
